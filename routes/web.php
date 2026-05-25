b602b18c8a54814fb417704756a9aa066118f6fe
bayugensoz <bayugensoz@gmail.com>
2026-05-25 20:08:34 +0700

    Init 2026-05-25

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KelurahanController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/kelurahan', [KelurahanController::class, 'index']);
